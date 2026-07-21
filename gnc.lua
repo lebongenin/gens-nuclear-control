c2b94625d2f7fb4160c92f3b989aa9e11f27cc4c
lebongenin <loic0genin@gmail.com>
2026-07-22 00:53:09 +0200

    Update gnc.lua

--- gnc.lua
+++ gnc.lua
@@ -1,7 +1,7 @@
 -- GEN'S Nuclear Control
 -- Installer and updater
 
-local VERSION = "0.0.5"
+local VERSION = "0.1.0"
 local BASE_URL =
     "https://raw.githubusercontent.com/lebongenin/gens-nuclear-control/main/"
 
